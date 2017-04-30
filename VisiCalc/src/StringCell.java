26877bafa92398daba3a0a4a589916c6e198add0
colbehr <colbehr@gmail.com>
2017-04-30 14:23:00 -0700

    Last Commit

---
+++ VisiCalc/src/StringCell.java
@@ -0,0 +1,20 @@
+/**
+ * AP Computer Science, Period 2
+ * Project 2 - VisiCalc2015
+ * Extra credit so far: 
+ * -Help (maybe)
+ * @author Colbehr
+ * @version March 2015
+ * Checkpoint 1, 2, and a little bit of 3
+ */
+public class StringCell extends Cell {
+
+
+	public StringCell(String userInput) {
+		super(userInput);
+	}
+
+	public String getDisplayValue() {
+		return null;
+	}
+}
